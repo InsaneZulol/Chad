3446c708bc33b6204f8b120b59b092b46a08cf3c
Alan Kałuża <senomz@gmail.com>
2020-06-10 23:51:42 +0200

    renaming

--- Chad/main.cpp
+++ Chad/main.cpp
@@ -12,10 +12,8 @@
 
 
 
-inline void throw_if_fail(HRESULT hr)
-{
-	if (FAILED(hr))
-	{
+inline void ThrowOnFail(HRESULT hr) {
+	if (FAILED(hr)) {
 		throw _com_error(hr);
 	}
 }
@@ -33,40 +31,40 @@ void PrintEndpointNames()
 	LPWSTR ptr_ep_id = nullptr;
 	try
 	{
-		throw_if_fail(ptr_enumerator.CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL));
+		ThrowOnFail(ptr_enumerator.CoCreateInstance(CLSID_MMDeviceEnumerator, NULL, CLSCTX_ALL));
 
-		throw_if_fail(ptr_enumerator->EnumAudioEndpoints(
+		ThrowOnFail(ptr_enumerator->EnumAudioEndpoints(
 			eRender, DEVICE_STATE_ACTIVE,
 			&ptr_collection));
 
-		UINT count;
-		throw_if_fail(ptr_collection->GetCount(&count));
-		if (count == 0) {
+		UINT num_devices;
+		ThrowOnFail(ptr_collection->GetCount(&num_devices));
+		if (num_devices == 0) {
 			std::wcout<<"No endpoints found." << std::endl;
 		}
 
-		for (UINT i = 0; i < count; i++) {
+		for (UINT i = 0; i < num_devices; i++) {
 			// Get pointer to endpoint number i.
-			throw_if_fail(ptr_collection->Item(i, &ptr_endpoint));
-			// Get the endpoint ID string.
-			throw_if_fail(ptr_endpoint->GetId(&ptr_ep_id));
-			throw_if_fail(ptr_endpoint->OpenPropertyStore(
+			ThrowOnFail(ptr_collection->Item(i, &ptr_endpoint));
+			// Get pointer to endpoint ID string.
+			ThrowOnFail(ptr_endpoint->GetId(&ptr_ep_id));
+			ThrowOnFail(ptr_endpoint->OpenPropertyStore(
 					STGM_READ, &ptr_propertory_store));
 
-			PROPVARIANT varName;
 			// Initialize container for property value.
-			PropVariantInit(&varName);
+			PROPVARIANT var_name;
+			PropVariantInit(&var_name);
 
 			// Get the endpoint's friendly-name property.
-			throw_if_fail(ptr_propertory_store->GetValue(
-				PKEY_Device_FriendlyName, &varName));
+			ThrowOnFail(ptr_propertory_store->GetValue(
+				PKEY_Device_FriendlyName, &var_name));
 
-			std::wcout << "Endpoint " << i << ": " << ptr_ep_id << " name: " << varName.pwszVal << std::endl;
+			std::wcout << "Endpoint " << i << ": " << ptr_ep_id << " name: " << var_name.pwszVal << std::endl;
 			ptr_endpoint.Release(); // non-safe relase?
 			ptr_propertory_store.Release(); // non-safe relase?
 			ptr_ep_id = nullptr;
 			CoTaskMemFree(ptr_ep_id);
-			PropVariantClear(&varName);
+			PropVariantClear(&var_name);
 		}
 	}
 	catch (_com_error err)
@@ -83,7 +81,7 @@ int main()
 	_setmode(_fileno(stdout), _O_U8TEXT); // from now on remember to either: a) only use wcout b) setmode to O_TEXT before couting
 	// initialize COM
 	try {
-		throw_if_fail(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+		ThrowOnFail(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
 			COINIT_DISABLE_OLE1DDE));
 
 		PrintEndpointNames();
